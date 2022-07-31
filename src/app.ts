63a7892f56f3d085237b436fcad0762a3c70ec06
dheeraj1447 <70004355+dheeraj1447@users.noreply.github.com>
2022-07-31 19:38:55 +0530

    Added xbrl file parser

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
-import { generate } from '@src/dynamodb';
+/* eslint-disable @typescript-eslint/no-var-requires */
+// import { generate } from '@src/dynamodb';
 
-generate();
+// generate();
+
+import { logger } from './logger';
+
+import axios from 'axios';
+import { parseXbrlFile } from 'xbrl-parser';
+
+import { writeFileSync } from 'fs';
+
+(async () => {
+  try {
+    const response = await axios
+      .get('https://archives.nseindia.com/corporate/xbrl/INDAS_83515_634675_13042022084056_WEB.xml', { responseType: 'text' });
+    const parsedDoc = await parseXbrlFile(response.data);
+    writeFileSync('/Users/dheeraj/Desktop/financials.json', JSON.stringify(parsedDoc));
+    logger.info(parsedDoc);
+  } catch (err) {
+    logger.error(err);
+  }
+})();
 
